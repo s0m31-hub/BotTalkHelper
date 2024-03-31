eef84fbef59896d0ca538f0a111960e3ec0606b4
s0m31 <admin@nwolfhub.org>
2024-03-31 15:45:05 +0300

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
-
+tasks.withType<Jar>() {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
 tasks.test {
     useJUnitPlatform()
 }
